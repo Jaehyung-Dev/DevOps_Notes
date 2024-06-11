96552e685cd9a105a94d80ea522d8f0dadcc8ba2
dev2024dev <157802843+dev2024dev@users.noreply.github.com>
2024-06-11 21:49:32 +0900

    Add files via upload

--- BankApplication.java
+++ BankApplication.java
@@ -1,5 +1,7 @@
 package chap00_practice;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class BankApplication {
@@ -8,25 +10,26 @@ public class BankApplication {
 		
 		Scanner sc = new Scanner(System.in);
 		Account account = new Account();
-		
+		List<Account> ac = new ArrayList[100];
 		while(true) {
 			System.out.println("--------------------------------------");
 			System.out.println("1.계좌생성|2.계좌목록|3.예금|4.출금|5.종료");
 			System.out.println("--------------------------------------");
 			System.out.print("선택>");
-			if(sc.nextInt() == 1) {
+			int num = sc.nextInt();
+			if(num == 1) {
 				account.createAccount();
 			}
-			else if(sc.nextInt() == 2) {
+			else if(num == 2) {
 				account.accountList();
 			}
-			else if(sc.nextInt() == 3) {
+			else if(num == 3) {
 				account.deposit();
 			}
-			else if(sc.nextInt() == 4) {
+			else if(num == 4) {
 				account.withdraw();
 			}
-			else if(sc.nextInt() == 5) {
+			else if(num == 5) {
 				System.out.println("프로그램을 종료합니다.");
 				break;
 			}
